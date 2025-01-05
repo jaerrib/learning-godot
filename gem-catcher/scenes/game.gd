6ab7dbe63ec526d475d1bf5d216064a1a39bb75d
jaerrib <jaerrib@tutanota.com>
2025-01-05 09:00:13 -0500

    Add gem spawning and paddle movement

---
+++ gem-catcher/scenes/game.gd
@@ -0,0 +1,28 @@
+extends Node2D
+
+
+@export var gem_scene: PackedScene
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	spawn_gem()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+	
+	
+func spawn_gem() -> void:
+	var new_gem: Gem = gem_scene.instantiate()
+	var xpos: float = randf_range(70, 1050)
+	new_gem.on_gem_off_screen.connect(game_over) 
+	new_gem.position = Vector2(xpos, -50)
+	add_child(new_gem)
+
+
+func game_over() -> void:
+	print("game_over")
+
+func _on_timer_timeout() -> void:
+	spawn_gem()
---
+++ gem-catcher/scenes/paddle.gd
@@ -0,0 +1,16 @@
+extends Area2D
+
+@export var speed: float = 200.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	position.x += speed * delta * Input.get_axis("left", "right")
+
+
+func _on_gem_area_entered(area: Area2D) -> void:
+	print(area)

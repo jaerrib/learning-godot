f31ed521e2f05c308aa6dd52f4135ff7a4e3c44a
jaerrib <jaerrib@tutanota.com>
2025-01-10 12:09:01 -0500

    Add pipe spawning, minor tweaks

--- tappy-plane/scenes/pipes/pipes.gd
+++ tappy-plane/scenes/pipes/pipes.gd
@@ -1,11 +1,14 @@
 extends Node2D
 
+class_name Pipes
+
+
 const SPEED = 120.0
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	position.x -= SPEED * delta
-	check_off_screen()
+	check_off_screen() 
 
 
 func check_off_screen() -> void:

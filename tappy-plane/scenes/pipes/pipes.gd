ea16ef2ccff10ccf4ade0f4172e3d99abb2da2f9
jaerrib <jaerrib@tutanota.com>
2025-01-16 12:05:53 -0500

    Remove hardcoded offscreen value

--- tappy-plane/scenes/pipes/pipes.gd
+++ tappy-plane/scenes/pipes/pipes.gd
@@ -2,9 +2,9 @@ extends Node2D
 
 class_name Pipes
 
-const OFF_SCREEN: float = -500.0 
 
 @onready var score_sound: AudioStreamPlayer2D = $ScoreSound
+@onready var von: VisibleOnScreenNotifier2D = $VisibleOnScreenNotifier2D
 
 
 func _ready() -> void:
@@ -18,16 +18,12 @@ func _process(delta: float) -> void:
 
 
 func check_off_screen() -> void:
-	if position.x < OFF_SCREEN :
+	if von.global_position.x < get_viewport_rect().position.x :
 		queue_free()
 
 
 func on_plane_died() -> void:
 	set_process(false)
-
-
-func _on_screen_exited() -> void:
-	queue_free()
 
 
 func _on_pipe_body_entered(body: Node2D) -> void:

d5efa31a65cb219d0a1f42e41cbbe53eb1040af1
jaerrib <jaerrib@tutanota.com>
2025-01-16 12:38:47 -0500

    Prevent immediate fall upon game start

--- tappy-plane/scenes/plane/plane.gd
+++ tappy-plane/scenes/plane/plane.gd
@@ -15,7 +15,7 @@ const POWER: float = -350.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	velocity = Vector2(0, POWER)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.

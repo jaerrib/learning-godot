ad10c3fc8e3bd577ab61a15e6e4e68c87245a588
jaerrib <jaerrib@tutanota.com>
2025-01-20 12:49:38 -0500

    Add drag vectors with limits to Animal

--- angry-animals/scenes/animal/animal.gd
+++ angry-animals/scenes/animal/animal.gd
@@ -4,12 +4,20 @@ extends RigidBody2D
 enum ANIMAL_STATE { READY, DRAG, RELEASE }
 
 
+const DRAG_LIM_MAX: Vector2 = Vector2(0, 60)
+const DRAG_LIM_MIN: Vector2 = Vector2(-60, 0)
+
+
+var _start: Vector2 = Vector2.ZERO
+var _drag_start: Vector2 = Vector2.ZERO
+var _dragged_vector: Vector2 = Vector2.ZERO
+
 var _state: ANIMAL_STATE = ANIMAL_STATE.READY
  
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	_start = position
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -22,7 +30,7 @@ func set_new_state(new_state: ANIMAL_STATE) -> void:
 	if _state == ANIMAL_STATE.RELEASE:
 		freeze = false 
 	elif _state == ANIMAL_STATE.DRAG:
-		pass
+		_drag_start = get_global_mouse_position()
 	
 	
 func detect_release() -> bool:
@@ -32,12 +40,23 @@ func detect_release() -> bool:
 			return true
 	return false
 	
-	 
+	
+func get_dragged_vector(gmp: Vector2) -> Vector2:
+	return gmp - _drag_start
+	
+
+func drag_in_limits() -> void:
+	_dragged_vector.x = clampf(_dragged_vector.x, DRAG_LIM_MIN.x, DRAG_LIM_MAX.x)
+	_dragged_vector.y = clampf(_dragged_vector.y, DRAG_LIM_MIN.y, DRAG_LIM_MAX.y)
+	position = _start + _dragged_vector
+ 
+
 func update_drag() -> void:
 	if detect_release():
-		pass
+		return
 	var gmp = get_global_mouse_position()
-	position = gmp
+	_dragged_vector = get_dragged_vector(gmp)
+	drag_in_limits()
 
 
 func update(delta: float) -> void:

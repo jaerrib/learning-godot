2964031ee733ae705b8fed504e7f279070c26a91
jaerrib <jaerrib@tutanota.com>
2025-01-21 12:18:07 -0500

    Add sound and rotating arrow when dragging

--- angry-animals/scenes/animal/animal.gd
+++ angry-animals/scenes/animal/animal.gd
@@ -11,12 +11,18 @@ const DRAG_LIM_MIN: Vector2 = Vector2(-60, 0)
 var _start: Vector2 = Vector2.ZERO
 var _drag_start: Vector2 = Vector2.ZERO
 var _dragged_vector: Vector2 = Vector2.ZERO
+var _last_dragged_vector: Vector2 = Vector2.ZERO
+
+@onready var stretch_sound: AudioStreamPlayer2D = $StretchSound
+@onready var arrow: Sprite2D = $Arrow
+
 
 var _state: ANIMAL_STATE = ANIMAL_STATE.READY
  
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	arrow.hide()
 	_start = position
 
 
@@ -28,11 +34,13 @@ func _physics_process(delta: float) -> void:
 func set_new_state(new_state: ANIMAL_STATE) -> void:
 	_state = new_state
 	if _state == ANIMAL_STATE.RELEASE:
+		arrow.hide()
 		freeze = false 
 	elif _state == ANIMAL_STATE.DRAG:
 		_drag_start = get_global_mouse_position()
-	
-	
+		arrow.show()
+
+
 func detect_release() -> bool:
 	if _state == ANIMAL_STATE.DRAG:
 		if Input.is_action_just_released("drag"):
@@ -41,11 +49,21 @@ func detect_release() -> bool:
 	return false
 	
 	
+func scale_arrow() -> void:
+	arrow.rotation = (_start - position).angle()
+
+
+func play_stretch_sound() -> void:
+	if(_last_dragged_vector - _dragged_vector).length() > 0:
+		if not stretch_sound.playing:
+			stretch_sound.play()
+
 func get_dragged_vector(gmp: Vector2) -> Vector2:
 	return gmp - _drag_start
 	
 
 func drag_in_limits() -> void:
+	_last_dragged_vector = _dragged_vector
 	_dragged_vector.x = clampf(_dragged_vector.x, DRAG_LIM_MIN.x, DRAG_LIM_MAX.x)
 	_dragged_vector.y = clampf(_dragged_vector.y, DRAG_LIM_MIN.y, DRAG_LIM_MAX.y)
 	position = _start + _dragged_vector
@@ -56,7 +74,9 @@ func update_drag() -> void:
 		return
 	var gmp = get_global_mouse_position()
 	_dragged_vector = get_dragged_vector(gmp)
+	play_stretch_sound()
 	drag_in_limits()
+	scale_arrow()
 
 
 func update(delta: float) -> void:

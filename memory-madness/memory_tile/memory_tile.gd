9efb171615f9b93a12ef689f2c2dfb5607def671
jaerrib <jaerrib@tutanota.com>
2025-02-04 12:26:49 -0500

    Set up memory titles from menu selection

---
+++ memory-madness/memory_tile/memory_tile.gd
@@ -0,0 +1,34 @@
+extends TextureButton
+
+
+class_name MemoryTitle
+
+
+@onready var frame_image: TextureRect = $FrameImage
+@onready var item_image: TextureRect = $ItemImage
+
+
+var _item_name: String
+var _can_select_me: bool = true
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+func reveal(r: bool) -> void:
+	frame_image.visible = r
+	item_image.visible = r
+
+
+func setup(image: ItemImage, frame: Texture2D) -> void:
+	frame_image.texture = frame
+	item_image.texture = image.get_texture()
+	_item_name = image.get_item_name()
+	reveal(false)
+
+
+func _on_pressed() -> void:
+	if _can_select_me:
+		reveal(true)
--- memory-madness/scenes/game/game.gd
+++ memory-madness/scenes/game/game.gd
@@ -1,6 +1,10 @@
 extends Control
 
 
+const MEMORY_TILE = preload("res://memory_tile/memory_tile.tscn")
+@onready var tc: GridContainer = $HB/MC/TC
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	SignalManager.on_level_selected.connect(on_level_selected)
@@ -11,9 +15,19 @@ func _process(delta: float) -> void:
 	pass
 
 
-func on_level_selected(level_num: int) -> void:
-	pass
+func add_memory_tile(image: ItemImage, frame: Texture2D) -> void:
+	var nt: MemoryTitle = MEMORY_TILE.instantiate()
+	tc.add_child(nt)
+	nt.setup(image, frame )
 
+func on_level_selected(level_num: int) -> void:
+	var ld: SelectedLevelData = GameManager.get_level_selectoin(level_num)
+	var frame: Texture2D = ImageManager.get_random_frame_image()
+	
+	tc.columns = ld.get_num_cols()
+	
+	for im in ld.get_selected_level_images():
+		add_memory_tile(im, frame)
 
 func _on_exit_button_pressed() -> void:
 	SignalManager.on_game_exit_pressed.emit()

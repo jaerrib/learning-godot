cf1e559098cfaa05f3b9ff0537f38f8efa4f31cc
jaerrib <jaerrib@tutanota.com>
2025-02-10 12:36:08 -0500

    Add horizontal player movement

--- foxy-antics/scenes/player/player.gd
+++ foxy-antics/scenes/player/player.gd
@@ -5,6 +5,12 @@ class_name Player
 
 
 const GRAVITY: float = 690.0
+const RUN_SPEED: float = 120.0
+const MAX_FALL: float = 400.0
+const JUMP_VELOCITY: float = -260.0
+
+
+@onready var sprite_2d: Sprite2D = $Sprite2D
 
 
 # Called when the node enters the scene tree for the first time.
@@ -16,4 +22,15 @@ func _ready() -> void:
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity.y += GRAVITY * delta
+	get_input()
 	move_and_slide()
+
+
+func get_input() -> void:
+	velocity.x = 0
+	if Input.is_action_pressed("left"):
+		velocity.x = -RUN_SPEED
+		sprite_2d.flip_h = true
+	elif Input.is_action_pressed("right"):
+		velocity.x = RUN_SPEED
+		sprite_2d.flip_h = false

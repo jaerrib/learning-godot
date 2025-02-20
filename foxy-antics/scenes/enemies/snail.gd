29d6063b9cab1702aa58419c5bb999218a9b78fe
jaerrib <jaerrib@tutanota.com>
2025-02-20 12:16:54 -0500

    Add snail logic

--- foxy-antics/scenes/enemies/snail.gd
+++ foxy-antics/scenes/enemies/snail.gd
@@ -1,11 +1,26 @@
 extends EnemyBase
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+@onready var floor_detection: RayCast2D = $FloorDetection
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
+
+@export var speed: float = 50.0
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _physics_process(delta: float) -> void:
+	super._physics_process(delta)
+	
+	if not is_on_floor():
+		velocity.y += _gravity * delta
+	else:
+		velocity.x = speed if animated_sprite_2d.flip_h else -speed
+	move_and_slide()
+	if is_on_floor():
+		if is_on_wall() or not floor_detection.is_colliding():
+			flip_me()
+
+
+func flip_me() -> void:
+	animated_sprite_2d.flip_h = !animated_sprite_2d.flip_h
+	floor_detection.position.x = -floor_detection.position.x

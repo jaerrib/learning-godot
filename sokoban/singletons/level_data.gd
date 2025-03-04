b91d1d404c82c3cd8048db07d7873018283a8ab9
jaerrib <jaerrib@tutanota.com>
2025-03-04 12:23:31 -0500

    Read in level data

--- sokoban/singletons/level_data.gd
+++ sokoban/singletons/level_data.gd
@@ -11,12 +11,17 @@ var _level_data: Dictionary = {}
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	load_level_data()
-	pass
 
+
+func setup_level(lns: String, raw_level__data: Dictionary) -> LevelLayout:
+	var layout: LevelLayout = LevelLayout.new()
+	var ps = raw_level__data.player_start
+	layout.set_player_start(ps.x, ps.y)
+	return layout
 
 func load_level_data() -> void:
 	var file = FileAccess.open(LEVEL_DATA_PATH, FileAccess.READ)
 	var raw_data = JSON.parse_string(file.get_as_text())
 
 	for lns in raw_data.keys():
-		_level_data[lns] = lns  + "_val"
+		_level_data[lns] = setup_level(lns, raw_data[lns])

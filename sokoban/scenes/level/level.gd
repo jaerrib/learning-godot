0214fab744507b328214cef2c607bf501369c16f
jaerrib <jaerrib@tutanota.com>
2025-03-05 12:44:33 -0500

    Place player on correct starting tile, autoplay idle animation

--- sokoban/scenes/level/level.gd
+++ sokoban/scenes/level/level.gd
@@ -24,8 +24,16 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-	
-	
+
+
+func place_player_on_tile(tile_coord: Vector2i) -> void:
+	var np: Vector2 = Vector2(
+		tile_coord.x * LevelData.TILE_SIZE,
+		tile_coord.y * LevelData.TILE_SIZE,
+	) + tile_layers.position
+	player.position = np
+
+
 func get_atlas_coord_for_layer_type(lt: TileLayers.LayerType) -> Vector2i:
 	match lt:
 		TileLayers.LayerType.FLOOR:
@@ -90,5 +98,7 @@ func setup_level() -> void:
 	setup_layer(TileLayers.LayerType.TARGET, targets_tiles, layout)
 	setup_layer(TileLayers.LayerType.BOX, boxes_tiles, layout)
 	setup_layer(TileLayers.LayerType.TARGET_BOX, boxes_tiles, layout)
+	
+	place_player_on_tile(layout.get_player_start())
 	
 	move_camera()

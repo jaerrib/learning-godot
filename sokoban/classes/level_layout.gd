2915b2109c19e3b5bd770713928259482a1efb7b
jaerrib <jaerrib@tutanota.com>
2025-03-05 06:10:40 -0500

    Build level data from JSON

--- sokoban/classes/level_layout.gd
+++ sokoban/classes/level_layout.gd
@@ -2,6 +2,15 @@ class_name LevelLayout
 
 
 var _player_start: Vector2i = Vector2i.ZERO
+var _tile_layers: TileLayers = TileLayers.new()
+
+
+func add_tile_to_layer(
+	tx: int,
+	ty: int,
+	lt: TileLayers.LayerType
+	) -> void:
+		_tile_layers.add_coord(Vector2i(tx, ty), lt)
 
 
 func set_player_start(tile_x: int, tile_y: int) -> void:
--- sokoban/singletons/level_data.gd
+++ sokoban/singletons/level_data.gd
@@ -13,10 +13,28 @@ func _ready() -> void:
 	load_level_data()
 
 
-func setup_level(lns: String, raw_level__data: Dictionary) -> LevelLayout:
+func add_tile_for_layer(
+	layout: LevelLayout,
+	layer_type: TileLayers.LayerType,
+	tile_coords: Array
+	) -> void:
+		for tile_coord in tile_coords:
+			layout.add_tile_to_layer(tile_coord.x, tile_coord.y, layer_type)
+
+
+func setup_level(lns: String, raw_level_data: Dictionary) -> LevelLayout:
 	var layout: LevelLayout = LevelLayout.new()
-	var ps = raw_level__data.player_start
+	var raw_tiles: Dictionary = raw_level_data.tiles
+	var ps = raw_level_data.player_start
+	
+	add_tile_for_layer(layout, TileLayers.LayerType.FLOOR, raw_tiles.Floor)
+	add_tile_for_layer(layout, TileLayers.LayerType.WALL, raw_tiles.Walls)
+	add_tile_for_layer(layout, TileLayers.LayerType.BOX, raw_tiles.Boxes)
+	add_tile_for_layer(layout, TileLayers.LayerType.TARGET_BOX, raw_tiles.TargetBoxes)
+	add_tile_for_layer(layout, TileLayers.LayerType.TARGET, raw_tiles.Targets)
+	
 	layout.set_player_start(ps.x, ps.y)
+	
 	return layout
 
 func load_level_data() -> void:

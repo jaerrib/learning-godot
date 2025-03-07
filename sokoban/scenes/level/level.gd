355fc3671369a9319a5d8fcae66ecc3af0e3d0eb
jaerrib <jaerrib@tutanota.com>
2025-03-07 12:23:05 -0500

    Finish navigation with reload and exit

--- sokoban/scenes/level/level.gd
+++ sokoban/scenes/level/level.gd
@@ -24,6 +24,11 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("reload"):
+		setup_level()
+	if Input.is_action_just_pressed("exit"):
+		GameManager.load_main_scene()
+	
 	var md: Vector2i = Vector2i.ZERO
 	if Input.is_action_just_pressed("right"):
 		md = Vector2i.RIGHT

361b9b627f79c5b596ec9b39b57741101f27ce50
jaerrib <jaerrib@tutanota.com>
2025-03-13 20:14:35 -0400

    Add flicker effect to shield

--- space-ace/scenes/shield/shield.gd
+++ space-ace/scenes/shield/shield.gd
@@ -8,12 +8,17 @@ class_name Shield
 @onready var timer: Timer = $Timer
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var sound: AudioStreamPlayer2D = $Sound
+@onready var sprite_2d: Sprite2D = $Sprite2D
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	enable_shield()
 
+
+func _process(delta: float) -> void:
+	var shield_flicker: float = randf_range(1.0, 2.0)
+	sprite_2d.self_modulate = Color(shield_flicker, shield_flicker, shield_flicker)
 
 func enable_shield() -> void:
 	collision_shape_2d.call_deferred("set_disabled", false)

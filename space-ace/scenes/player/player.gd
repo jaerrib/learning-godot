1e2485fec02e45baf387d7a2b48357a658c62ffa
jaerrib <jaerrib@tutanota.com>
2025-03-18 12:07:42 -0400

    Add powerup detection

--- space-ace/scenes/player/player.gd
+++ space-ace/scenes/player/player.gd
@@ -9,6 +9,7 @@ class_name Player
 
 @onready var sprite_2d: Sprite2D = $Sprite2D
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var shield: Shield = $Shield
 
 
 const MARGIN: float = 16.0
@@ -47,3 +48,12 @@ func set_limits() -> void:
 	var vp: Rect2 = get_viewport_rect()
 	_lower_right = Vector2(vp.size.x - MARGIN, vp.size.y - MARGIN)
 	_upper_left = Vector2(MARGIN, MARGIN)
+
+
+func _on_area_entered(area: Area2D) -> void:
+	if area is PowerUp:
+		match area.get_Power_up_type():
+			PowerUp.PowerUpType.HEALTH:
+				print("health")
+			PowerUp.PowerUpType.SHIELD:
+				shield.enable_shield()

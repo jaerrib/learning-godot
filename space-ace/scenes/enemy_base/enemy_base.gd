271b6abb32ff78553645d0d3e7d27ab5e67cec11
jaerrib <jaerrib@tutanota.com>
2025-03-19 12:40:11 -0400

    Randomize enemy animation selection

--- space-ace/scenes/enemy_base/enemy_base.gd
+++ space-ace/scenes/enemy_base/enemy_base.gd
@@ -4,12 +4,15 @@ extends PathFollow2D
 class_name EnemeyBase
 
 
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
+
 var _speed: float = 50.0
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	SpaceUtils.play_random_animation(animated_sprite_2d)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.

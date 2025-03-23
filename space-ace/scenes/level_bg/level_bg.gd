921fa4ed599252c783d6a33380a64c456669f220
jaerrib <jaerrib@tutanota.com>
2025-03-23 09:25:37 -0400

    Shake camera when player is damaged

--- space-ace/scenes/level_bg/level_bg.gd
+++ space-ace/scenes/level_bg/level_bg.gd
@@ -4,6 +4,13 @@ extends ParallaxBackground
 const SPEED: float = 50
 
 
+@onready var parallax_layer: ParallaxLayer = $ParallaxLayer
+@onready var parallax_layer_2: ParallaxLayer = $ParallaxLayer2
+@onready var parallax_layer_3: ParallaxLayer = $ParallaxLayer3
+
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	scroll_offset.y += SPEED * delta
+	parallax_layer.motion_offset.y += SPEED * delta * 0.2
+	parallax_layer_2.motion_offset.y += SPEED * delta * 0.3
+	parallax_layer_3.motion_offset.y += SPEED * delta * 0.33

b21321e5615119d89f8026ef66c85c31b35b6d1a
jaerrib <jaerrib@tutanota.com>
2025-03-30 15:53:57 -0400

    Add version number to main screen

--- space-ace/scenes/main/main.gd
+++ space-ace/scenes/main/main.gd
@@ -1,7 +1,7 @@
 extends Control
 
 
-@onready var high_score_label: Label = $MarginContainer/HighScoreLabel
+@onready var high_score_label: Label = $MarginContainer/HBoxContainer/HighScoreLabel
 
 
 # Called when the node enters the scene tree for the first time.

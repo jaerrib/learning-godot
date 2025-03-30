618c57fa71a4c56baf1a88655fa7aebc12e55cae
jaerrib <jaerrib@tutanota.com>
2025-03-30 07:12:02 -0400

    Add handling of saucer death

--- space-ace/scenes/saucer/saucer.gd
+++ space-ace/scenes/saucer/saucer.gd
@@ -5,6 +5,8 @@ class_name Saucer
 
 
 const SPEED: float = 20
+const BOOM_DELAY: float = 0.1
+const SCORE: int = 150
 const WAIT_TIME: float = 7.0
 const WAIT_VAR: float = 1.0
 const PB: String = "parameters/playback"
@@ -13,6 +15,9 @@ const PB: String = "parameters/playback"
 @onready var s_mach: AnimationNodeStateMachinePlayback = tree[PB]
 @onready var shoot_timer: Timer = $ShootTimer
 @onready var sound: AudioStreamPlayer2D = $Sound
+@onready var hit_box: HitBox = $HitBox
+@onready var health_bar: HealthBar = $HealthBar
+@onready var booms: Node2D = $Booms
 
 
 var _shooting: bool = false
@@ -32,6 +37,11 @@ func _process(delta: float) -> void:
 		queue_free()
 
 
+func make_booms() -> void:
+	for b in booms.get_children():
+		SignalManager.on_create_explosion.emit(b.global_position, Explosion.ExplosionType.BOOM)
+		await get_tree().create_timer(BOOM_DELAY).timeout
+
 func stop_shooting() -> void:
 	_shooting = false
 	reset_timer()
@@ -49,3 +59,18 @@ func reset_timer() -> void:
 
 func _on_shoot_timer_timeout() -> void:
 	shoot()
+
+
+func _on_health_bar_died() -> void:
+	health_bar.hide()
+	make_booms()
+	set_process(false)
+	shoot_timer.stop()
+	hit_box.deactivate()
+	s_mach.travel("die")
+	ScoreManager.increment_score(SCORE)
+
+
+func _on_hit_box_area_entered(area: Area2D) -> void:
+	if area  is HitBox:
+		health_bar.take_damage(area.get_damage())
